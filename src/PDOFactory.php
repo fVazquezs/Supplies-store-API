0c37838786ad1ae604ab8ace2bca355e776b81ef
Fernando Vázquez <vzs.fernando@gmail.com>
2019-05-15 14:31:59 -0300

    adding to heroku

--- src/PDOFactory.php
+++ src/PDOFactory.php
@@ -7,7 +7,7 @@ class PDOFactory
     public static function getConnection()
     {
         if (!isset($pdo)) {
-            $pdo = new PDO("mysql:host=localhost;dbname=supplies", "root", "");
+            $pdo = new PDO("mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_60d4b49d28d0f61", "b0de910d8ab6f3", "31876c1b");
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             $pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
